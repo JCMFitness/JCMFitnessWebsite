7680500ab458ed04faf0bced68abe8315fbc8f95
Justin Holsclaw <jholsclaw@live.com>
2021-03-01 16:26:49 -0700

    Add project files.

---
+++ JCMFitnessWebsite/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace JCMFitnessWebsite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public Timer _timer;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void SetTimer(double interval)
+        {
+            _timer = new Timer(interval);
+            _timer.Elapsed += NotifyTimerElapsed;
+            _timer.Enabled = true;
+        }
+        public event Action OnElapsed;
+
+        private void NotifyTimerElapsed(Object source, ElapsedEventArgs e)
+        {
+            OnElapsed?.Invoke();
+            _timer.Dispose();
+        }
+    }
+}
