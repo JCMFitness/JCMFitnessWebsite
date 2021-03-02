a88c3bf34500146b0d00e5052ef8c0e1bf635367
Maksad Annamuradov <maksad.annamuradov@students.snow.edu>
2021-03-02 16:25:35 -0700

    Added working download button

--- JCMFitnessWebsite/wwwroot/js/site.js
+++ JCMFitnessWebsite/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+let downloadButton = document.querySelector('.button');
+if (downloadButton) {
+    downloadButton.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        /* Start loading process. */
+        downloadButton.classList.add('loading');
+
+        /* Set delay before switching from loading to success. */
+        window.setTimeout(function () {
+            downloadButton.classList.remove('loading');
+            downloadButton.classList.add('success');
+        }, 3000);
+
+        /* Reset animation. */
+        window.setTimeout(function () {
+            downloadButton.classList.remove('success');
+        }, 8000);
+    });
+};
